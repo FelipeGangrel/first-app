5416efa4e170ce00c007f0390bff0ed06a0ba741
Felipe Gangrel <felipefrog@gmail.com>
2016-09-12 10:12:08 -0300

    teste

--- src/app/other/another.component.ts
+++ src/app/other/another.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'myapp-another',
-  template: '<article><ng-content></ng-content></article>',
+  template: `
+  <article>
+    <ng-content></ng-content>
+  </article>`,
   styles: ['article{border: 1px solid #000; padding: 15px; margin-bottom: 5px}']
 })
 export class AnotherComponent {
